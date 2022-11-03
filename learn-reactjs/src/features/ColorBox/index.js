8e50349695f97d26aee29326cd146ae9bc4e6f28
boyhotkey96 <boyhotkey96@gmail.com>
2022-11-04 03:11:44 +0700

    add ColorBox using useState & localStorage: change color + change prevcolor

---
+++ learn-reactjs/src/features/ColorBox/index.js
@@ -0,0 +1,48 @@
+import "./ColorBox.scss";
+import { useState } from "react";
+
+// variable save Prev indexRandom
+let prevIndexRandom = null;
+const getRandomColor = () => {
+    const initColor = ["black", "yellow", "blue", "green", "red"];
+    let indexRandom;
+    
+    // handle avoid repeat indexRandom
+    do {
+        indexRandom = Math.floor(Math.random() * initColor.length);
+    } while (prevIndexRandom === indexRandom);
+    prevIndexRandom = indexRandom;
+
+    return initColor[indexRandom];
+};
+
+function ColorBox() {
+    const [color, setColor] = useState(() => {
+        return localStorage.getItem("color-box") || "deeppink";
+    });
+
+    const [prevColor, setPrevColor] = useState('#999');
+
+    const handleBoxClick = () => {
+        setPrevColor(color);
+
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem("color-box", newColor);
+    };
+
+    return (
+        <div className="container">
+            <h1>Change color box local storage</h1>
+            <div
+                className="box-color"
+                style={{ backgroundColor: color, color: prevColor }}
+                onClick={handleBoxClick}
+            >
+                Color Box
+            </div>
+        </div>
+    );
+}
+
+export default ColorBox;
--- learn-reactjs/src/index.js
+++ learn-reactjs/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ColorBox from './features/ColorBox';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ColorBox />
     </BrowserRouter>
   </React.StrictMode>
 );
