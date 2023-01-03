8220efba1a690a3ed7e96b9d8d1c09d38f53ae5b
boyhotkey96 <boyhotkey96@gmail.com>
2023-01-03 12:03:03 +0700

    update todo features: sync filters to URL params

--- learn-reactjs/src/features/Todo/index.js
+++ learn-reactjs/src/features/Todo/index.js
@@ -1,60 +1,88 @@
+import queryString from "query-string";
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import TodoList from "./components/TodoList";
 
 function TodoFeature() {
-    const initTodoList = [
-        { id: 1, title: "code", status: "new" },
-        { id: 2, title: "eat", status: "completed" },
-        { id: 3, title: "ngủ", status: "new" },
-    ];
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const [todoList, setTodoList] = useState(initTodoList);
-    const [filteredStatus, setFilteredStatus] = useState("all");
+  const initTodoList = [
+    { id: 1, title: "code", status: "new" },
+    { id: 2, title: "eat", status: "completed" },
+    { id: 3, title: "ngủ", status: "new" },
+  ];
 
-    const handleClick = (todo) => {
-        // console.log(todo);
-        // clone current todoList to the new one
-        const newTodoList = [...todoList];
-        const index = newTodoList.findIndex((x) => x.id === todo.id);
-        if (index < 0) return;
+  const [todoList, setTodoList] = useState(initTodoList);
+  const [filteredStatus, setFilteredStatus] = useState(() => {
+    const param = queryString.parse(location.search);
+    return param.status || "all";
+  });
 
-        newTodoList[index] = {
-            ...newTodoList[index],
-            status: newTodoList[index].status === "new" ? "completed" : "new",
-        };
+  useEffect(() => {
+    const queryParams = location.search;
+    const params = queryString.parse(queryParams);
+    setFilteredStatus(params.status || "all");
+  }, [location.search]);
 
-        setTodoList(newTodoList);
+  const handleClick = (todo) => {
+    // console.log(todo);
+    // clone current todoList to the new one
+    const newTodoList = [...todoList];
+    const index = newTodoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    newTodoList[index] = {
+      ...newTodoList[index],
+      status: newTodoList[index].status === "new" ? "completed" : "new",
     };
 
-    const handleClickShowAll = () => {
-        setFilteredStatus("all");
-    };
-    const handleClickShowCompleted = () => {
-        setFilteredStatus("completed");
-    };
-    const handleClickShowNew = () => {
-        setFilteredStatus("new");
-    };
+    setTodoList(newTodoList);
+  };
 
-    const renderTodoList = todoList.filter(
-        (todo) => filteredStatus === "all" || filteredStatus === todo.status
-    );
+  const handleClickShowAll = () => {
+    // setFilteredStatus("all");
 
-    useEffect(() => {
-        console.log("todoList: ", todoList);
-    }, [todoList]);
+    const queryParams = { status: "all" };
+    const params = queryString.stringify(queryParams);
+    navigate(`${location.pathname}?${params}`);
+  };
 
-    return (
-        <div>
-            <h2>Todo List</h2>
-            <TodoList todoList={renderTodoList} onTodoClick={handleClick} />
-            <div>
-                <button onClick={handleClickShowAll}>Show All</button>
-                <button onClick={handleClickShowCompleted}>Show Completed</button>
-                <button onClick={handleClickShowNew}>Show New</button>
-            </div>
-        </div>
-    );
+  const handleClickShowCompleted = () => {
+    // setFilteredStatus("completed");
+
+    const queryParams = { status: "completed" };
+    const params = queryString.stringify(queryParams);
+    navigate(`${location.pathname}?${params}`);
+  };
+
+  const handleClickShowNew = () => {
+    // setFilteredStatus("new");
+
+    const queryParams = { status: "new" };
+    const params = queryString.stringify(queryParams);
+    navigate(`${location.pathname}?${params}`);
+  };
+
+  const renderTodoList = todoList.filter(
+    (todo) => filteredStatus === "all" || filteredStatus === todo.status
+  );
+
+  useEffect(() => {
+    console.log("todoList: ", todoList);
+  }, [todoList]);
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <TodoList todoList={renderTodoList} onTodoClick={handleClick} />
+      <div>
+        <button onClick={handleClickShowAll}>Show All</button>
+        <button onClick={handleClickShowCompleted}>Show Completed</button>
+        <button onClick={handleClickShowNew}>Show New</button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoFeature;
